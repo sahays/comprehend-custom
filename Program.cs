dbe9a9d3f8f8df87883a42796cd3cbf4638dd69f
Sanjeet <sanjeet@earthrise.local>
2019-07-03 17:22:58 -0700

    readme

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.Comprehend;
 using Amazon.Comprehend.Model;
@@ -8,8 +7,10 @@ using Microsoft.Extensions.Configuration;
 
 namespace comprehend_custom {
 	class Program {
-		const string DataAccessRoleArn = "arn:aws:iam::865118636886:role/comprehend-s3";
-		const string TrainingFile = "s3://s6jackjack/JEOPARDY_CSV-labeled.csv";
+		const string ServiceRoleArn = "<your-IAM-service-role-for-Comprehend>";
+		const string TrainingFile = "s3://<your-s3-bucket>/jeopardy-filtered-labeled.csv";
+		const string InputFile = "s3://<your-s3-bucket>/JEOPARDY_CSV.csv";
+		const string OutputLocation = "s3://<your-s3-bucket>/comprehend-output/";
 		static void Main(string[] args) {
 			var awsOptions = BuildAwsOptions();
 			var service = new ComprehendService(awsOptions.CreateServiceClient<IAmazonComprehend>());
@@ -17,15 +18,16 @@ namespace comprehend_custom {
 			// create a custom classifier using training data
 			var jobName = Guid.NewGuid().ToString();
 			Console.WriteLine(jobName);
-			var task = service.CreateCustomClassifier(jobName, "en", DataAccessRoleArn, TrainingFile);
-			task.Wait();
-			var taskArn = task.Result.DocumentClassifierArn;
-			Console.WriteLine(taskArn);
-			service.WaitForCreationCompletion(taskArn);
+			var newCustomClassifierArn = service.CreateCustomClassifier(jobName, "en", ServiceRoleArn, TrainingFile);
+			Console.WriteLine(newCustomClassifierArn);
+			service.WaitForCreationCompletion(newCustomClassifierArn);
 			Console.WriteLine("custom classifier created");
 
 			// run a batch job on unlabeled data using the classifier you just created
-
+			var jobId = service.StartBatchJob(jobName, ServiceRoleArn, newCustomClassifierArn, InputFile, OutputLocation);
+			Console.WriteLine(jobId);
+			service.WaitForJobCompletion(jobId);
+			Console.WriteLine("job complete");
 		}
 
 		private static AWSOptions BuildAwsOptions() {
@@ -43,9 +45,11 @@ namespace comprehend_custom {
 			this.comprehend = comprehend;
 		}
 
-		public async Task<StartDocumentClassificationJobResponse> StartBatchJob(string jobName, string inputS3Uri, string outputS3Uri) {
-			return await this.comprehend.StartDocumentClassificationJobAsync(new StartDocumentClassificationJobRequest {
+		public string StartBatchJob(string jobName, string roleArn, string customClassifierArn, string inputS3Uri, string outputS3Uri) {
+			using(var task = this.comprehend.StartDocumentClassificationJobAsync(new StartDocumentClassificationJobRequest {
 				JobName = jobName,
+				DataAccessRoleArn = roleArn,
+				DocumentClassifierArn = customClassifierArn,
 				InputDataConfig = new InputDataConfig {
 					InputFormat = InputFormat.ONE_DOC_PER_LINE,
 					S3Uri = inputS3Uri
@@ -53,18 +57,24 @@ namespace comprehend_custom {
 				OutputDataConfig = new OutputDataConfig {
 					S3Uri = outputS3Uri
 				}
-			});
+			})) {
+				task.Wait();
+				return task.Result.JobId;
+			}
 		}
 
-		public async Task<CreateDocumentClassifierResponse> CreateCustomClassifier(string jobName, string languageCode, string roleArn, string inputS3Uri) {
-			return await this.comprehend.CreateDocumentClassifierAsync(new CreateDocumentClassifierRequest {
+		public string CreateCustomClassifier(string jobName, string languageCode, string roleArn, string inputS3Uri) {
+			using(var task = this.comprehend.CreateDocumentClassifierAsync(new CreateDocumentClassifierRequest {
 				DocumentClassifierName = jobName,
 				LanguageCode = languageCode,
 				DataAccessRoleArn = roleArn,
 				InputDataConfig = new DocumentClassifierInputDataConfig {
 					S3Uri = inputS3Uri
 				}
-			});
+			})) {
+				task.Wait();
+				return task.Result.DocumentClassifierArn;
+			}
 		}
 
 		public bool IsCreationComplete(string jobArn) {
@@ -72,11 +82,22 @@ namespace comprehend_custom {
 				DocumentClassifierArn = jobArn
 			});
 			task.Wait();
-			var status = task.Result.DocumentClassifierProperties.Status.Value;
-			if(status == "IN_ERROR") {
-				Console.WriteLine("Error: [{0}]", task.Result.DocumentClassifierProperties.Message);
-			}
+			var result = task.Result;
+			var status = result.DocumentClassifierProperties.Status.Value;
+			Print(status, result);
 			return status == "IN_ERROR" || status == "TRAINED";
+		}
+
+		private void Print(string status, DescribeDocumentClassifierResponse result) {
+			if(status == "IN_ERROR") {
+				Console.WriteLine("Error: [{0}]", result.DocumentClassifierProperties.Message);
+			} else if(status == "TRAINED") {
+				var props = result.DocumentClassifierProperties;
+				var metrics = result.DocumentClassifierProperties.ClassifierMetadata.EvaluationMetrics;
+				Console.WriteLine("Status: [{0}], Message: [{1}]", props.Status, props.Message);
+				Console.WriteLine("Started at: [{0}], completed at: [{1}]", props.TrainingStartTime, props.TrainingStartTime);
+				Console.WriteLine("Accuracy: [{0}], F1Score: [{1}], Precision: [{2}], Recall: [{3}]", metrics.Accuracy, metrics.F1Score, metrics.Precision, metrics.Recall);
+			}
 		}
 
 		public bool IsBatchJobComplete(string jobId) {
@@ -85,7 +106,20 @@ namespace comprehend_custom {
 			});
 			task.Wait();
 			var status = task.Result.DocumentClassificationJobProperties.JobStatus;
+			var result = task.Result;
+			Print(status, result);
 			return status == "FAILED" || status == "COMPLETED";
+		}
+
+		private void Print(string status, DescribeDocumentClassificationJobResponse result) {
+			if(status == "FAILED") {
+				Console.WriteLine("Error: [{0}]", result.DocumentClassificationJobProperties.Message);
+			} else if(status == "COMPLETED") {
+				var props = result.DocumentClassificationJobProperties;
+				Console.WriteLine("Job Id: [{0}], Name: [{1}], Status: [{2}], Message: [{3}]", props.JobId, props.JobName, props.JobStatus, props.Message);
+				Console.WriteLine("Started at: [{0}], completed at: [{1}]", props.SubmitTime, props.EndTime);
+				Console.WriteLine("Output located at: [{0}]", props.OutputDataConfig.S3Uri);
+			}
 		}
 
 		public void WaitForCreationCompletion(string jobArn, int delay = 5000) {
--- README.md
+++ README.md
@@ -1,12 +1,36 @@
-# comprehend-custom
-.NET Core C# code samples for Amazon Comprehend Custom Classification
+# About
+.NET Core C# code samples for [Amazon Comprehend](https://aws.amazon.com/comprehend/) Custom Classification. You can use Amazon Comprehend to build your own models for custom classification, assigning a document to a class or a category.
 
-IAM Role ARN that is used as ```DataAccessRoleArn``` for ```CreateDocumentClassifier```
-```
-arn:aws:iam::<account-id>:role/comprehend-s3
-```
+# Overview
+Custom classification is a two step process. First you train a custom classifier to recognize the categories that are of interest to you. To train the classifier, you send Amazon Comprehend a group of labeled documents. After Amazon Comprehend builds the classifier, you send documents to be classified. The custom classifier examines each document and returns the label that best represents the content of the document.
 
-```DataAccessRoleArn``` uses the following policy document to grant privileges to Amazon Comprehend to access the S3 bucket where training data is stored
+In this sample we have done the following:
+- Used Amazon Comprehend to create a [Custom Classifier](https://docs.aws.amazon.com/comprehend/latest/dg/how-document-classification-training.html)
+- Used Amazon Comprehend custom classifier to categorize unlabeled documents in a test file (each line is a document) by starting a [classification job](https://docs.aws.amazon.com/comprehend/latest/dg/how-class-run.html)
+
+
+# Configuration
+| Variable  | Purpose  | File |
+|---|---|---|---|---|
+| ServiceRoleArn |  IAM Service Role for Amazon Comprehend that needs to read/write from S3 buckets | Program.cs |
+| TrainingFile | This file has labeled data that is used by Comprehend to train the custom classifier  | Program.cs |
+| InputFile  | This file has unlabeled data that is used as an input for the Comprehend classification batch job  | Program.cs  |
+| OutputLocation | This is the S3 bucket where the Comprehend classification batch job output will be emitted | Program.cs |
+
+# Prerequisites
+- [Dotnet Core 2.2](https://dotnet.microsoft.com/download/dotnet-core/2.2)
+- [AWS CLI](https://docs.aws.amazon.com/polly/latest/dg/setup-aws-cli.html) for
+  running AWS CLI commands after configuring a
+  [default or named profile](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
+
+# Steps to execute
+- Download the code
+- [create a new S3 bucket](https://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html) for training and unlabeled data 
+- [create an IAM role](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-service.html) using the policy document described below
+- go to Program.cs, find all variables with `<your->` and replace them with actual values in your own test account
+- From a command line, go to the downloaded folder and then execute `dotnet run` this will download all dependencies, build, and run the code
+
+```ServiceRoleArn``` uses the following policy document to grant privileges to Amazon Comprehend to access the S3 bucket where training data is stored
 ```json
 {
     "Version": "2012-10-17",
@@ -32,12 +56,31 @@ arn:aws:iam::<account-id>:role/comprehend-s3
     ]
 }
 ```
-# Errors
-Classification failure error
+
+At the completion of the training you'll see a response similar to the following:
+```
+Status: [TRAINED], Message: []
+Started at: [7/3/19 9:52:14 PM], completed at: [7/3/19 9:52:14 PM]
+Accuracy: [0.9149], F1Score: [0.8674], Precision: [0.8901], Recall: [0.8489]
+custom classifier created
+```
+
+Job completion response output print
+```
+Job Id: [8df6e23b534a9c7aa2831e58cbef04ac], Name: [06df74c8-c5ba-4325-a8e1-9ba5c54eeea5], Status: [COMPLETED], Message: []
+Started at: [7/3/19 9:33:33 PM], completed at: [7/3/19 9:40:13 PM]
+Output located at: [s3://<your-bucket-name>/<some-object-key>/<your-account-id>-CLN-8df6e23b534a9c7aa2831e58cbef04ac/output/output.tar.gz]
+```
+
+# Troubleshooting
+In case you encounter a Classification failure error like the following, please ensure that the S3 bucket is in the same region as Comprehend
 ```
 INPUT_BUCKET_NOT_IN_SERVICE_REGION: The provided input S3 bucket is not in the service region.
 ```
-Another error
+If you get the following error, then please note that each classification can have up to a maximum of 1000 unique labels. The sample training file that I have used `jeopardy-filtered-labeled.csv` has only 3 unique labels each having more than 1000 documents (each line is a document). Read [Training a Custom Classifier](https://docs.aws.amazon.com/comprehend/latest/dg/how-document-classification-training.html) for more information
 ```
 Error: [Found 27983 unique labels. The maximum allowed number of unique labels is 1000.]
 ```
+
+# Reference
+Source of the file [JEOPARDY_CSV.csv](https://drive.google.com/file/d/0BwT5wj_P7BKXb2hfM3d2RHU1ckE/view?usp=sharing) from [this website](https://blog.cambridgespark.com/50-free-machine-learning-datasets-natural-language-processing-d88fb9c5c8da) 
